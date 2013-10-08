5c993765068bcf29626d9f451d4eb4ebb8bff306
Stefan Huber <MSNexploder@gmail.com>
2013-10-08 16:20:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bytes::Application.config.secret_key_base = '1e1dec0e5474895a98d0e319e987da5b222cf7a89c9eb17e7f47a41d2b05cadba9c7ab27ae1721a6491b396a23fca74c96d1aa590a8298326846632cc844f63f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bytes::Application.config.session_store :cookie_store, key: '_bytes_session'
